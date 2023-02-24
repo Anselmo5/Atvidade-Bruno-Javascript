562761105515df4bc403730a4385240b3e86c387
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-02-24 09:56:52 -0300

    Create lex.js

---
+++ lex.js
@@ -0,0 +1,85 @@
+// let carro ={
+//     portas:4,
+//     vendido:true,
+//     valor: 239.00,
+//     marca:'BMW',
+//     acessiriso: [ 'Ar condicionario', 'tv', 'freio', 'nitro'
+
+//     ]
+// }
+// console.log(carro);
+// console.log(carro.marca)
+// console.log(carro.valor)
+// console.log(carro.vendido)
+
+// // Uma forma de saida
+
+// console.log('O '+carro+'Custa R$'+valor)
+ 
+// AT1
+
+let nome ={
+    idade: 17,
+    altura: 1.80,
+    Cor: negro
+}
+
+
+// AT2
+
+let pessoa ={
+    Nomopessoa: Anselmo,
+}
+
+console.log(pessoa.Nomopessoa);
+
+// AT3
+
+let Gabriel={
+    // Dados da primeira pessoa
+
+    nome: Maria,
+    idade: 17,
+
+    // fim dos Dados da primeira pessoa
+}
+
+let Bruno={
+ // Dados Segunda pessoa
+ Nome: joão,
+ idade:16,
+  // Fim dos Dados da Segunda pessoa
+}
+
+
+
+if (Gabriel.idade>Bruno.idade) {
+    console.log('Você e o Mais velho');
+} else if (Bruno.idade>Gabriel.idade){
+        console.log('Você e o Mais velho');
+} else{
+    console.log('N');
+}
+
+
+// AT4
+
+
+let funcionarioAnselmo ={
+    nome: Anselmo,
+    salario: 6300.00
+}
+
+
+let funcionarioAlex ={
+    nome: Alex,
+    salario:6700.00
+}
+
+let media = (funcionarioAlex.salari+funcionarioAnselmo.salario )/2;
+console.log(funcionarioAlex.salario);
+console.log(funcionarioAnselmo.salario);
+console.log(media);
+
+
+// AT5
